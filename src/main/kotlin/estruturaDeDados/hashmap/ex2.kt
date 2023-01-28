915fe1ea928ecc825f3681c698f44f2edf3c46ba
João Vitor A <joaovitoraredes.jvv@gmail.com>
2023-01-28 18:40:00 -0300

    exemplos hashmap

---
+++ src/main/kotlin/estruturaDeDados/hashmap/ex2.kt
@@ -0,0 +1,42 @@
+package estruturaDeDados.hashmap
+
+import java.util.LinkedList
+import kotlin.random.Random
+import kotlin.system.measureNanoTime
+
+val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+fun randomString() = (1..10)
+    .map { Random.nextInt(0, charPool.size).let { charPool[it] } }
+    .joinToString("")
+
+fun buildRandomUser(): User {
+    val name = randomString()
+    val phone = randomString()
+    return User(name, phone)
+}
+
+data class User(
+    val name: String,
+    val phone: String
+)
+
+fun main() {
+    val userHashMap = HashMap<String, User>()
+    val userArrayList = LinkedList<User>()
+
+    repeat(10_000_000) {
+        val user = buildRandomUser()
+        userHashMap.put(user.phone, user)
+        userArrayList.add(user)
+    }
+
+    val user = userArrayList[9_000_000]
+
+    val hashTime = measureNanoTime { userHashMap.get(user.phone) }
+    val listTime = measureNanoTime {
+        userArrayList.firstOrNull { it.phone == user.phone }
+    }
+
+    println(hashTime)
+    println(listTime)
+}
---
+++ src/main/kotlin/estruturaDeDados/hashmap/ex3.kt
@@ -0,0 +1,24 @@
+package estruturaDeDados.hashmap
+
+//data class Event(
+//    val title: String,
+//    val time: Long,
+//    val day: Int
+//)
+//
+//data class Calendar(
+//    private val hashmap: HashMap<Int, MutableList<Event>> = HashMap()
+//) {
+//
+//    fun newEvent(event: Event) {
+//        if (hashmap.containsKey(event.day)) {
+//            hashmap[event.day]?.add(event)
+//        } else {
+//            hashmap[event.day] = mutableListOf(event)
+//        }
+//    }
+//
+//    fun getEventsForTheDay(day: Int): List<Event> {
+//        return hashmap[day] ?: emptyList()
+//    }
+//}
